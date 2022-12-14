c146ca46d8bb4940b827f458e807bc414a451987
Jon Scarpelli <scarpelli.jon@gmail.com>
2022-12-14 16:54:38 -0600

    tweak

--- seeders/20221209054159-Bark.js
+++ seeders/20221209054159-Bark.js
@@ -3,9 +3,9 @@ const falso = require('@ngneat/falso')
 // /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    let barks = [...Array(80)].map((_) => ({
-      handle: falso.randUserName(),
-      bark: falso.randQuote(),
+    let barks = [...Array(40)].map((_) => ({
+      bark: falso.randUserName(),
+      handle: falso.randCatchPhrase(),
       barker: falso.randNumber({min:1, max: 12}),
       barked: falso.randNumber({min:1, max: 12}),
       createdAt: new Date(),
