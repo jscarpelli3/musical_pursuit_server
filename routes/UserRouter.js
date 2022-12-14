2c63679e2c66c91ce77183908106de0276882fe3
Jon Scarpelli <scarpelli.jon@gmail.com>
2022-12-14 10:52:23 -0600

    bloacked backed routes

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -5,50 +5,48 @@ const middleware = require('../middleware')
 //Get User With their list of people they watch(i.e their competiton)
 router.get(
   '/userprof/:userId',
-  // middleware.stripToken,
-  // middleware.verifyToken,
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.getUserWithWatched
 )
 
 //Get All Users
 router.get('/users',
-// middleware.stripToken,
-  // middleware.verifyToken,
   controller.getUsers
 )
 
 //Get a single user 
 router.get('/:userId',
-// middleware.stripToken,
-// middleware.verifyToken,
+middleware.stripToken,
+middleware.verifyToken,
 controller.getUser
 )
 
 //Delete a single user 
 router.delete('/:userId',
-// middleware.stripToken,
-// middleware.verifyToken,
+middleware.stripToken,
+middleware.verifyToken,
 controller.deleteUser
 )
 
 //Delete a a watchlist record
 router.delete('/:being_watchedId/:watcherId',
-// middleware.stripToken,
-// middleware.verifyToken,
+middleware.stripToken,
+middleware.verifyToken,
 controller.deleteFromWatchlist
 )
 
 router.post(
   '/watch',
-  // middleware.stripToken,
-  // middleware.verifyToken,
+  middleware.stripToken,
+  middleware.verifyToken,
   controller.addToWatchlist
 )
 
 //Update a users stats 
 router.put('/:userId',
-// middleware.stripToken,
-// middleware.verifyToken,
+middleware.stripToken,
+middleware.verifyToken,
 controller.updateStats
 )
 
